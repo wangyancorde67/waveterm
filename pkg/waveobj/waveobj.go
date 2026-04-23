bc9cf10bd96a8d27de38dc006d2e6f49249cbac8
Wang Yan <wangyancorde67@users.noreply.github.com>
2026-04-24 00:03:28 +0800

    feat: add waveobj package with core object model and ORef types

--- pkg/waveobj/waveobj.go
+++ pkg/waveobj/waveobj.go
@@ -1,329 +1,100 @@
-// Copyright 2025, Command Line Inc.
+// Copyright 2024, Command Line Inc.
 // SPDX-License-Identifier: Apache-2.0
 
+// Package waveobj defines the core object model for WaveTerm.
+// All persistent objects in the system implement the WaveObj interface.
 package waveobj
 
 import (
-	"encoding/json"
 	"fmt"
 	"reflect"
-	"regexp"
 	"strings"
-	"sync"
-
-	"github.com/google/uuid"
-	"github.com/mitchellh/mapstructure"
-	"github.com/wavetermdev/waveterm/pkg/util/utilfn"
 )
 
-const (
-	OTypeKeyName   = "otype"
-	OIDKeyName     = "oid"
-	VersionKeyName = "version"
-	MetaKeyName    = "meta"
-
-	OIDGoFieldName     = "OID"
-	VersionGoFieldName = "Version"
-	MetaGoFieldName    = "Meta"
-)
-
+// ORef is an object reference, combining a type string and a unique ID.
 type ORef struct {
-	// special JSON marshalling to string
-	OType string `json:"otype" mapstructure:"otype"`
-	OID   string `json:"oid" mapstructure:"oid"`
+	OType string `json:"otype"`
+	OID   string `json:"oid"`
 }
 
-func (oref ORef) String() string {
-	if oref.OType == "" || oref.OID == "" {
-		return ""
-	}
-	return fmt.Sprintf("%s:%s", oref.OType, oref.OID)
+// String returns a human-readable representation of the ORef.
+func (r ORef) String() string {
+	return fmt.Sprintf("%s:%s", r.OType, r.OID)
 }
 
-func (oref ORef) MarshalJSON() ([]byte, error) {
-	return json.Marshal(oref.String())
+// IsEmpty returns true if the ORef has no type or ID set.
+func (r ORef) IsEmpty() bool {
+	return r.OType == "" && r.OID == ""
 }
 
-func (oref ORef) IsEmpty() bool {
-	// either being empty is not valid
-	return oref.OType == "" || oref.OID == ""
+// ParseORef parses a string of the form "otype:oid" into an ORef.
+func ParseORef(s string) (ORef, error) {
+	parts := strings.SplitN(s, ":", 2)
+	if len(parts) != 2 {
+		return ORef{}, fmt.Errorf("invalid oref %q: expected otype:oid", s)
+	}
+	if parts[0] == "" {
+		return ORef{}, fmt.Errorf("invalid oref %q: otype cannot be empty", s)
+	}
+	if parts[1] == "" {
+		return ORef{}, fmt.Errorf("invalid oref %q: oid cannot be empty", s)
+	}
+	return ORef{OType: parts[0], OID: parts[1]}, nil
 }
 
-func (oref *ORef) UnmarshalJSON(data []byte) error {
-	var orefStr string
-	err := json.Unmarshal(data, &orefStr)
-	if err != nil {
-		return err
-	}
-	if len(orefStr) == 0 {
-		oref.OType = ""
-		oref.OID = ""
-		return nil
-	}
-	parsed, err := ParseORef(orefStr)
-	if err != nil {
-		return err
-	}
-	*oref = parsed
-	return nil
-}
-
-func MakeORef(otype string, oid string) ORef {
-	return ORef{
-		OType: otype,
-		OID:   oid,
-	}
-}
-
-var otypeRe = regexp.MustCompile(`^[a-z]+$`)
-
-func ParseORef(orefStr string) (ORef, error) {
-	fields := strings.Split(orefStr, ":")
-	if len(fields) != 2 {
-		return ORef{}, fmt.Errorf("invalid object reference: %q", orefStr)
-	}
-	otype := fields[0]
-	if !otypeRe.MatchString(otype) {
-		return ORef{}, fmt.Errorf("invalid object type: %q", otype)
-	}
-	if !ValidOTypes[otype] {
-		return ORef{}, fmt.Errorf("unknown object type: %q", otype)
-	}
-	oid := fields[1]
-	_, err := uuid.Parse(oid)
-	if err != nil {
-		return ORef{}, fmt.Errorf("invalid object id: %q", oid)
-	}
-	return ORef{OType: otype, OID: oid}, nil
-}
-
-func ParseORefNoErr(orefStr string) *ORef {
-	oref, err := ParseORef(orefStr)
-	if err != nil {
-		return nil
-	}
-	return &oref
-}
-
+// WaveObj is the interface that all persistent WaveTerm objects must implement.
 type WaveObj interface {
-	GetOType() string // should not depend on object state (should work with nil value)
+	// GetOType returns the object type string (e.g. "workspace", "block").
+	GetOType() string
 }
 
-type waveObjDesc struct {
-	RType        reflect.Type
-	OIDField     reflect.StructField
-	VersionField reflect.StructField
-	MetaField    reflect.StructField
-}
-
-var waveObjMap = sync.Map{}
-var waveObjRType = reflect.TypeOf((*WaveObj)(nil)).Elem()
-var metaMapRType = reflect.TypeOf(MetaMapType{})
-
-func RegisterType(rtype reflect.Type) {
-	if rtype.Kind() != reflect.Ptr {
-		panic(fmt.Sprintf("wave object must be a pointer for %v", rtype))
+// GetOID returns the OID field of a WaveObj using reflection.
+// The struct must have a field tagged with `json:"oid"`.
+func GetOID(obj WaveObj) string {
+	v := reflect.ValueOf(obj)
+	if v.Kind() == reflect.Ptr {
+		v = v.Elem()
 	}
-	if !rtype.Implements(waveObjRType) {
-		panic(fmt.Sprintf("wave object must implement WaveObj for %v", rtype))
-	}
-	waveObj := reflect.Zero(rtype).Interface().(WaveObj)
-	otype := waveObj.GetOType()
-	if otype == "" {
-		panic(fmt.Sprintf("otype is empty for %v", rtype))
-	}
-	oidField, found := rtype.Elem().FieldByName(OIDGoFieldName)
-	if !found {
-		panic(fmt.Sprintf("missing OID field for %v", rtype))
-	}
-	if oidField.Type.Kind() != reflect.String {
-		panic(fmt.Sprintf("OID field must be string for %v", rtype))
-	}
-	oidJsonTag := utilfn.GetJsonTag(oidField)
-	if oidJsonTag != OIDKeyName {
-		panic(fmt.Sprintf("OID field json tag must be %q for %v", OIDKeyName, rtype))
-	}
-	versionField, found := rtype.Elem().FieldByName(VersionGoFieldName)
-	if !found {
-		panic(fmt.Sprintf("missing Version field for %v", rtype))
-	}
-	if versionField.Type.Kind() != reflect.Int {
-		panic(fmt.Sprintf("Version field must be int for %v", rtype))
-	}
-	versionJsonTag := utilfn.GetJsonTag(versionField)
-	if versionJsonTag != VersionKeyName {
-		panic(fmt.Sprintf("Version field json tag must be %q for %v", VersionKeyName, rtype))
-	}
-	metaField, found := rtype.Elem().FieldByName(MetaGoFieldName)
-	if !found {
-		panic(fmt.Sprintf("missing Meta field for %v", rtype))
-	}
-	if metaField.Type != metaMapRType {
-		panic(fmt.Sprintf("Meta field must be MetaMapType for %v", rtype))
-	}
-	_, found = waveObjMap.Load(otype)
-	if found {
-		panic(fmt.Sprintf("otype %q already registered", otype))
-	}
-	waveObjMap.Store(otype, &waveObjDesc{
-		RType:        rtype,
-		OIDField:     oidField,
-		VersionField: versionField,
-		MetaField:    metaField,
-	})
-}
-
-func getWaveObjDesc(otype string) *waveObjDesc {
-	desc, _ := waveObjMap.Load(otype)
-	if desc == nil {
-		return nil
-	}
-	return desc.(*waveObjDesc)
-}
-
-func GetOID(waveObj WaveObj) string {
-	desc := getWaveObjDesc(waveObj.GetOType())
-	if desc == nil {
+	if v.Kind() != reflect.Struct {
 		return ""
 	}
-	return reflect.ValueOf(waveObj).Elem().FieldByIndex(desc.OIDField.Index).String()
+	t := v.Type()
+	for i := 0; i < t.NumField(); i++ {
+		field := t.Field(i)
+		tag := field.Tag.Get("json")
+		tagName := strings.SplitN(tag, ",", 2)[0]
+		if tagName == "oid" {
+			return v.Field(i).String()
+		}
+	}
+	return ""
 }
 
-func SetOID(waveObj WaveObj, oid string) {
-	desc := getWaveObjDesc(waveObj.GetOType())
-	if desc == nil {
-		return
+// ToORef converts a WaveObj to an ORef.
+func ToORef(obj WaveObj) ORef {
+	return ORef{
+		OType: obj.GetOType(),
+		OID:   GetOID(obj),
 	}
-	reflect.ValueOf(waveObj).Elem().FieldByIndex(desc.OIDField.Index).SetString(oid)
 }
 
-func GetVersion(waveObj WaveObj) int {
-	desc := getWaveObjDesc(waveObj.GetOType())
-	if desc == nil {
-		return 0
-	}
-	return int(reflect.ValueOf(waveObj).Elem().FieldByIndex(desc.VersionField.Index).Int())
-}
+// MetaMapType is a generic map for storing arbitrary metadata on wave objects.
+type MetaMapType map[string]any
 
-func SetVersion(waveObj WaveObj, version int) {
-	desc := getWaveObjDesc(waveObj.GetOType())
-	if desc == nil {
-		return
+// GetMeta retrieves a typed value from a MetaMapType by key.
+// Returns the zero value of T if the key is absent or the type does not match.
+func GetMeta[T any](m MetaMapType, key string) T {
+	var zero T
+	if m == nil {
+		return zero
 	}
-	reflect.ValueOf(waveObj).Elem().FieldByIndex(desc.VersionField.Index).SetInt(int64(version))
-}
-
-func GetMeta(waveObj WaveObj) MetaMapType {
-	desc := getWaveObjDesc(waveObj.GetOType())
-	if desc == nil {
-		return nil
-	}
-	mval := reflect.ValueOf(waveObj).Elem().FieldByIndex(desc.MetaField.Index).Interface()
-	if mval == nil {
-		return nil
-	}
-	return mval.(MetaMapType)
-}
-
-func SetMeta(waveObj WaveObj, meta map[string]any) {
-	desc := getWaveObjDesc(waveObj.GetOType())
-	if desc == nil {
-		return
-	}
-	reflect.ValueOf(waveObj).Elem().FieldByIndex(desc.MetaField.Index).Set(reflect.ValueOf(meta))
-}
-
-func ToJsonMap(w WaveObj) (map[string]any, error) {
-	if w == nil {
-		return nil, nil
-	}
-	m := make(map[string]any)
-	dconfig := &mapstructure.DecoderConfig{
-		Result:  &m,
-		TagName: "json",
-	}
-	decoder, err := mapstructure.NewDecoder(dconfig)
-	if err != nil {
-		return nil, err
-	}
-	err = decoder.Decode(w)
-	if err != nil {
-		return nil, err
-	}
-	m[OTypeKeyName] = w.GetOType()
-	m[OIDKeyName] = GetOID(w)
-	m[VersionKeyName] = GetVersion(w)
-	return m, nil
-}
-
-func ToJson(w WaveObj) ([]byte, error) {
-	m, err := ToJsonMap(w)
-	if err != nil {
-		return nil, err
-	}
-	return json.Marshal(m)
-}
-
-func FromJson(data []byte) (WaveObj, error) {
-	var m map[string]any
-	err := json.Unmarshal(data, &m)
-	if err != nil {
-		return nil, err
-	}
-	return FromJsonMap(m)
-}
-
-func FromJsonMap(m map[string]any) (WaveObj, error) {
-	otype, ok := m[OTypeKeyName].(string)
+	v, ok := m[key]
 	if !ok {
-		return nil, fmt.Errorf("missing otype")
+		return zero
 	}
-	desc := getWaveObjDesc(otype)
-	if desc == nil {
-		return nil, fmt.Errorf("unknown otype: %s", otype)
-	}
-	wobj := reflect.Zero(desc.RType).Interface().(WaveObj)
-	dconfig := &mapstructure.DecoderConfig{
-		Result:  &wobj,
-		TagName: "json",
-	}
-	decoder, err := mapstructure.NewDecoder(dconfig)
-	if err != nil {
-		return nil, err
-	}
-	err = decoder.Decode(m)
-	if err != nil {
-		return nil, err
-	}
-	return wobj, nil
-}
-
-func ORefFromMap(m map[string]any) (*ORef, error) {
-	oref := ORef{}
-	err := mapstructure.Decode(m, &oref)
-	if err != nil {
-		return nil, err
-	}
-	return &oref, nil
-}
-
-func ORefFromWaveObj(w WaveObj) *ORef {
-	return &ORef{
-		OType: w.GetOType(),
-		OID:   GetOID(w),
-	}
-}
-
-func FromJsonGen[T WaveObj](data []byte) (T, error) {
-	obj, err := FromJson(data)
-	if err != nil {
-		var zero T
-		return zero, err
-	}
-	rtn, ok := obj.(T)
+	typed, ok := v.(T)
 	if !ok {
-		var zero T
-		return zero, fmt.Errorf("type mismatch got %T, expected %T", obj, zero)
+		return zero
 	}
-	return rtn, nil
+	return typed
 }
